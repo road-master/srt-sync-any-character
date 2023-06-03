c69f50ade69146b2f26ea38601eaf4c1a3af7828
Master <roadmasternavi@gmail.com>
2023-06-03 18:02:41 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# srt-sync-any-character
-SrtSync が文字化けする問題を解決するためのツールです
+# SrtSync Any Character
+
+[SrtSync] が文字化けする問題を解決するためのツールです  
+[SrtSync] は AviUtl で動画に行ったカット編集と同じように字幕をカットするツールです
+
+## インストール
+
+1\.  
+Windows に Python をインストールします:
+
+```powershell
+choco install -y python
+```
+
+2\.  
+このリポジトリーから `srt_sync.py` をダウンロードします
+
+3\.  
+[DTV補完所] ー＞DTV から `SrtSync0.1.9` をダウンロードし、  
+`srt_sync.py` と同じフォルダに `SrtSync.exe` を配置します
+
+4\.  
+[AviUtlプラグイン置き場] にある `自動フィールドシフト インタレース解除プラグイン ver7.5a` をダウンロードし、  
+[AviUtl] に `del_import.auf` をインストールします
+
+参考: [AviUtlと拡張編集プラグインの導入方法【ダウンロード･インストール】 | AviUtlの易しい使い方](https://aviutl.info/dl-innsuto-ru/)
+
+## 使い方
+
+1\.  
+AviUtl でカット編集前の ts ファイルから字幕を抽出します:
+
+```powershell
+ffmpeg -fix_sub_duration -i original.ts -c:s text sub.srt
+```
+
+2\.  
+AviUtl でカット編集します
+
+3\.  
+AviUtl から削除リストをエクスポートします:
+
+[ファイル] -> [エクスポート] -> [削除リスト]
+
+4\.  
+削除リストと字幕を srt_sync.py に渡します:
+
+```powershell
+python srt_sync.py remove_list.txt sub.srt
+```
+
+すると、カットされた字幕が出力されます
+
+5\.  
+AviUtl でカット編集後の mp4 ファイルに字幕を合成します:
+
+```powershell
+ffmpeg -i cut.mp4 -i sub_cut.srt -c copy -c:s mov_text -metadata:s:s:0 language=jpn subtitled.mp4
+```
+
+[SrtSync]: http://www2.wazoku.net/2sen/
+[DTV補完所]: http://www2.wazoku.net/2sen/
+[AviUtlプラグイン置き場]: https://aji0.web.fc2.com/
+[AviUtl]: http://spring-fragrance.mints.ne.jp/aviutl/
---
+++ srt_sync.py
@@ -0,0 +1,106 @@
+from dataclasses import dataclass
+import subprocess
+import sys
+from pathlib import Path
+from typing import Callable
+
+
+@dataclass
+class Sub:
+    index: int
+    time: str
+    text: str
+
+
+Subs = dict[int, Sub]
+
+
+def clear_file(file: str) -> Path:
+    path = Path(file)
+    if path.exists():
+        path.unlink()
+    return path
+
+
+def load(file_sub: Path) -> Subs:
+    subs = {}
+    try:
+        with file_sub.open(encoding="utf-8") as file:
+            while True:
+                line = next(file).strip()
+                if not line.isdigit():
+                    continue
+                index = int(line)
+                subs[index] = Sub(index, next(file), next(file))
+    except StopIteration:
+        pass
+    return subs
+
+
+def save(
+    file_path: Path,
+    subs: Subs,
+    what_write_to_text: Callable[[Sub], str],
+) -> None:
+    with file_path.open("w", encoding="utf-8") as file:
+        for sub in subs.values():
+            file.write(str(sub.index) + "\n")
+            file.write(sub.time)
+            file.write(what_write_to_text(sub))
+            file.write("\n")
+
+
+class SrtSync:
+    def __init__(self, file_input: Path) -> None:
+        self.input = file_input
+        self.output = clear_file(f"{self.input.stem}_new.srt")
+
+    def cut(self, remove_list: Path) -> Path:
+        executable = Path('SrtSync.exe').resolve()
+        process = subprocess.Popen(
+            [executable, "-aviutldl", remove_list, self.input],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        while process.poll() is None:
+            line = process.stdout.readline().strip()
+            print(line)
+            if line.startswith('続行するには何かキーを押してください'):
+                process.communicate('\r\n')
+        if not self.output.exists():
+            raise Exception("Failed to cut")
+        return self.output
+
+
+class SrtSyncWrapper:
+    @classmethod
+    def process(cls, file_sub_original: Path, remove_list: Path) -> None:
+        file_sub_temporary = cls.replace_text_with_index(file_sub_original)
+        file_sub_cut = SrtSync(file_sub_temporary).cut(remove_list)
+        cls.replace_index_with_text(file_sub_cut, file_sub_original)
+
+    @staticmethod
+    def replace_text_with_index(file_sub_original: Path) -> Path:
+        subs_original = load(file_sub_original)
+        file_sub_temporary = clear_file(f"{file_sub_original.stem}_replaced_by_index.srt")
+        save(file_sub_temporary, subs_original, lambda sub: f"\\{sub.index}\\\n")
+        return file_sub_temporary
+
+    @staticmethod
+    def replace_index_with_text(file_sub_cut: Path, file_sub_original: Path) -> None:
+        file_sub_cut_fixed = clear_file(f"{file_sub_original.stem}_cut.srt")
+        subs_cut = load(file_sub_cut)
+        subs_original = load(file_sub_original)
+        save(
+            file_sub_cut_fixed,
+            subs_cut,
+            lambda sub: subs_original[int(sub.text.replace("\\", ""))].text
+        )
+
+
+if __name__ == "__main__":
+    remove_list = Path(sys.argv[1])
+    sub_original = Path(sys.argv[2])
+    SrtSyncWrapper.process(sub_original, remove_list)
